43e43391283b9e2cd620c4da1a6992846ae6fd29
tumilok <vladislav.tumilovich@gmail.com>
2020-03-14 00:34:09 +0100

    added procedure and view

--- 01-oracle/procedures.sql
+++ 01-oracle/procedures.sql
@@ -1,41 +1,80 @@
 -- PROCEDURES/FUNCTIONS RECEIVING DATA
 
-CREATE OR REPLACE TYPE wycieczki_info_type
+-- Uczestnicy Wycieczki
+CREATE OR REPLACE TYPE type_wycieczka_info
     AS OBJECT (
         ID_WYCIECZKI    INT,
         NAZWA           VARCHAR2(50),
-        KRAJ            DATE,
-        "DATA"          VARCHAR2(100),
+        KRAJ            VARCHAR2(100),
+        "DATA"          DATE,
         IMIE            VARCHAR2(50),
         NAZWISKO        VARCHAR2(50),
         STATUS          CHAR(1)
     );
 
-CREATE OR REPLACE TYPE wycieczki_info_table_type
-    IS TABLE OF wycieczki_info_type;
-
+CREATE OR REPLACE TYPE type_wycieczka_info_table
+    IS TABLE OF type_wycieczka_info;
 
 CREATE OR REPLACE
-FUNCTION uczestnicy_wycieczki(id INT)
-  return wycieczki_info_table_type as result_table wycieczki_info_table_type;
-  is_table INTEGER;
-  BEGIN
-    SELECT COUNT(*) INTO is_table FROM WYCIECZKI w
-        WHERE w.ID_WYCIECZKI = id;
+FUNCTION fnc_uczestnicy_wycieczki(id INT)
+    return type_wycieczka_info_table as result_table type_wycieczka_info_table;
+    is_table INTEGER;
+    BEGIN
+        SELECT COUNT(*) INTO is_table FROM WYCIECZKI w
+            WHERE w.ID_WYCIECZKI = id;
 
-    IF is_table = 0 THEN
-      raise_application_error(-20014, 'Brak wycieczki o podanym id');
-    END IF;
+        IF is_table = 0 THEN
+          raise_application_error(-20014, 'Brak wycieczki o podanym id');
+        END IF;
 
-    SELECT wycieczki_info_type(rw.ID_WYCIECZKI, rw.KRAJ, rw.DATA, rw.NAZWA, rw.IMIE,
-                             rw.NAZWISKO, rw.STATUS)
-        BULK COLLECT INTO result_table
-    FROM REZERWACJEWSZYSTKIE rw
-    WHERE rw.ID_WYCIECZKI = id AND rw.STATUS <> 'A';
-    return result_table;
-  end uczestnicy_wycieczki;
+        SELECT type_wycieczka_info(rw.ID_WYCIECZKI, rw.NAZWA, rw.KRAJ, rw.DATA, rw.IMIE,
+                                        rw.NAZWISKO, rw.STATUS)
+            BULK COLLECT INTO result_table
+        FROM VIEW_REZERWACJE_WSZYSTKIE rw
+        WHERE rw.ID_WYCIECZKI = id AND rw.STATUS <> 'A';
+
+        RETURN result_table;
+    END fnc_uczestnicy_wycieczki;
+
+SELECT * from TABLE(fnc_uczestnicy_wycieczki(3));
+
+-- Rezerwacje Osoby
+CREATE OR REPLACE TYPE type_osoba_info
+    AS OBJECT (
+        ID_OSOBY    INT,
+        IMIE        VARCHAR2(50),
+        NAZWISKO    VARCHAR2(50),
+        KONTAKT     VARCHAR2(100),
+        PESEL       VARCHAR2(11),
+        NAZWA       VARCHAR2(50),
+        KRAJ        VARCHAR2(100),
+        "DATA"      DATE,
+        STATUS      CHAR(1)
+    );
 
 
+CREATE OR REPLACE TYPE type_osoba_info_table
+    IS TABLE OF type_osoba_info;
 
-SELECT * from TABLE(uczestnicy_wycieczki(4));
+CREATE OR REPLACE
+FUNCTION fnc_rezerwacje_osoby(id INT)
+    return type_osoba_info_table as result_table type_osoba_info_table;
+    is_table INTEGER;
+    BEGIN
+        SELECT COUNT(*) INTO is_table FROM OSOBY w
+            WHERE w.ID_OSOBY = id;
 
+        IF is_table = 0 THEN
+          raise_application_error(-20014, 'Brak osoby o podanym id');
+        END IF;
+
+        SELECT type_osoba_info(wo.ID_OSOBY, wo.IMIE, wo.NAZWISKO, wo.KONTAKT, wo.PESEL,
+                                    wo.NAZWA, wo.KRAJ, wo.DATA, wo.STATUS)
+            BULK COLLECT INTO result_table
+        FROM VIEW_WYCIECZKI_OSOBY wo
+        WHERE wo.ID_OSOBY = id;
+
+        RETURN result_table;
+    END fnc_rezerwacje_osoby;
+
+SELECT * from TABLE(FNC_REZERWACJE_OSOBY(1));
--- 01-oracle/views.sql
+++ 01-oracle/views.sql
@@ -52,3 +52,19 @@ CREATE OR REPLACE VIEW view_wycieczki_dostepne
         FROM view_wycieczki_miejsca wm
         WHERE wm.LICZBA_WOLNYCH_MIEJSC > 0
             AND wm.DATA > CURRENT_DATE;
+
+CREATE OR REPLACE VIEW view_wycieczki_osoby
+    AS
+        SELECT
+            o.ID_OSOBY,
+            o.IMIE,
+            o.NAZWISKO,
+            o.KONTAKT,
+            o.PESEL,
+            w.NAZWA,
+            w.KRAJ,
+            w.DATA,
+            r.STATUS
+        FROM OSOBY o
+            JOIN REZERWACJE r on o.ID_OSOBY = r.ID_OSOBY
+            JOIN WYCIECZKI w on r.ID_WYCIECZKI = w.ID_WYCIECZKI;
